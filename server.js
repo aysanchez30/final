28f201f1b60f8e3c447f73fc24f06054818d7709
Ava Sanchez <ava@Avas-MacBook-Air-2.local>
2023-12-12 23:23:21 -0500

    fix

--- server.js
+++ server.js
@@ -12,62 +12,7 @@ const upload = multer({ dest: __dirname + "/public/images" });
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/scheduling.html");
 });
-const animals = [
-{
-    _id: 1,
-    name: "Lion",
-    color: "Brown",
-    size: "8-9ft long",
-    located: ["Africa, India"],
-    diet: "Carnivore",
-    image: "images/lion.avif "
-},
-{
-    _id: 2,
-    name: "Penguin",
-    color: "Black and White",
-    size: "3-4ft",
-    located: ["Antarctica", "Southern Hemisphere"],
-    diet: "Carnivore",
-    image: "images/penguin.jpeg",
-},
-{
-    _id: 3,
-    name: "Dolphin",
-    color: "Gray",
-    size: "6-7ft",
-    located: ["Oceans worldwide"],
-    diet: "Carnivore",
-    image: "images/dolphin.jpeg",
-},
-{
-    _id: 4,
-    name: "Elephant",
-    color: "Gray",
-    size: "Up to 13ft tall",
-    located: ["Africa", "Asia"],
-    diet: "Herbivore",
-    image: "images/elephant.jpeg",
-},
-{
-    _id: 5,
-    name: "Koala",
-    color: "Gray",
-    size: "About 2 feet tall",
-    located: "Australia",
-    diet: "Herbivore",
-    image: "images/koala.jpeg",
-  },
-  {
-    _id: 6,
-    name: "Clownfish",
-    color: "Orange with black and white stripes",
-    size: "Up to 4 inches",
-    located: ["Coral reefs in the Pacific and Indian Oceans"],
-    diet: "Omnivore",
-    image: "images/clownfish.jpeg",
-  }
-];
+
 
 app.get("/api/animals", (req, res) => {
     res.send(animals);
