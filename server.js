352853e041e89a84a2079b389df38a521912370a
Ava Sanchez <ava@Avas-MacBook-Air-2.local>
2023-12-13 03:01:13 -0500

    idea

--- server.js
+++ server.js
@@ -1,102 +1,67 @@
 const express = require("express");
 const app = express();
-const Joi = require("joi");
-const multer = require("multer");
-app.use(express.static("public"));
-app.use(express.json());
-const cors = require("cors");
-app.use(cors());
+const bodyParser = require("body-parser");
 
-const upload = multer({ dest: __dirname + "/public/images" });
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const appointments = [];
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/scheduling.html");
 });
 
-
-app.get("/api/animals", (req, res) => {
-    res.send(animals);
+app.get("/api/appointments", (req, res) => {
+    res.send(appointments);
 });
 
-app.post("/api/animals", upload.single("img"), (req, res) => {
-    const result = validateAnimal(req.body);
+app.post("/api/schedule-appointment", (req, res) => {
+    const { type, time } = req.body;
 
-    if (result.error) {
-        res.status(400).send(result.error.details[0].message);
-        return;
+    if (!type || !time) {
+        return res.status(400).send("Type and time are required.");
     }
 
-    const animal = {
-        _id: animals.length + 1,
-        name: req.body.name,
-        color: req.body.color,
-        size: req.body.size,
-        located: req.body.located.split(","),
-        diet: req.body.diet
-    }
+    const appointment = {
+        _id: appointments.length + 1,
+        type: type,
+        time: time,
+    };
 
-    if (req.file) {
-        animal.img = "images/" + req.file.filename;
-    }
-
-    animals.push(animal);
-    res.send(animals);
+    appointments.push(appointment);
+    res.json(appointments);
 });
 
-app.put("/api/animals/:id", upload.single("img"), (req, res) => {
+
+app.put("/api/edit-appointment/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const { type, time } = req.body;
+
+    if (appointment && type && time) {
+        appointment.type = type;
+        appointment.time = time;
+        res.json(appointment);
+    } else {
+        res.status(400).send("Invalid input or appointment not found.");
+    }
+});
+
+app.delete("/api/delete-appointment/:id", (req, res) => {
     const id = parseInt(req.params.id);
 
-    const animal = animals.find((r) => r._id === id);;
+    const appointmentIndex = appointments.findIndex((a) => a._id === id);
 
-    const result = validateAnimal(req.body);
-
-    if (result.error) {
-        res.status(400).send(result.error.details[0].message);
-        return;
+    if (appointmentIndex !== -1) {
+        const deletedAppointment = appointments.splice(appointmentIndex, 1);
+        res.json(deletedAppointment[0]);
+    } else {
+        res.status(404).send("Appointment not found.");
     }
-
-    animal.name = req.body.name;
-    animal.color = req.body.color;
-    animal.size = req.body.size;
-    animal.located = req.body.located.split(",");;
-    animal.diet = req.body.diet;
-
-    if (req.file) {
-        animal.img = "images/" + req.file.filename;
-    }
-
-    res.send(animal);
 });
 
-app.delete("/api/animals/:id", upload.single("img"), (req, res) => {
-    const id = parseInt(req.params.id);
 
-    const animal = animals.find((r) => r._id === id);
-
-    if (!animal) {
-        res.status(404).send("The animal was not found");
-        return;
-    }
-
-    const index = animals.indexOf(animal);
-    animals.splice(index, 1);
-    res.send(animal);
-
-});
-
-const validateAnimal = (animal) => {
-    const schema = Joi.object({
-        _id: Joi.number().allow(''),
-        name: Joi.string().allow(''),
-        color: Joi.string().min(3).required(),
-        size: Joi.string().min(3).required(),
-        located: Joi.string().min(3).required(),
-        diet: Joi.string().min(3).required()
-    });
-
-    return schema.validate(animal);
-};
 
 app.listen(3000, () => {
-    console.log("I'm listening");
+    console.log("I'm live");
 });
